2b95e6fcd59d93589df6f6bdc411ea4266951900
Tomáš Kafka <tk@tomaskafka.com>
2025-01-26 00:08:47 +0100

    markdown test

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,42 @@
 import React, { useState } from "react";
+import Markdown from "react-markdown";
+import { tokens } from "./tokens.ts";
+
+const markdownExample = `
+# This should be a title
+
+And this paragraph
+
+- a list
+- of items
+            `;
 
 function App(): JSX.Element {
   const [count, setCount] = useState<number>(0);
 
   return (
     <div className="min-h-screen bg-secondary-50">
-      <main className="container mx-auto px-4 py-8">
-        <h1 className="text-4xl font-bold text-gray-900 mb-8 text-center">
-          Vite + React
-        </h1>
-        <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden p-6">
+      <main className={tokens.container}>
+        <h1 className={tokens.h1}>Vite + React</h1>
+        <div className={tokens.card}>
           <button
             onClick={() => setCount((count) => count + 1)}
-            className="w-full bg-primary-500 hover:bg-primary-600 text-white font-semibold py-2 px-4 rounded-lg transition-colors duration-200"
+            className={tokens.buttonPrimary}
           >
             Count is {count}
           </button>
-          <p className="mt-4 text-gray-600">
+          <p className="my-4 text-gray-600">
             Edit{" "}
             <code className="bg-primary-100 rounded px-1">src/App.tsx</code> and
             save to test HMR
           </p>
+
+          <div className="space-y-8">
+            <h2 className={tokens.h2}>Markdown test</h2>
+            <Markdown>{markdownExample}</Markdown>
+          </div>
         </div>
+
         <p className="text-gray-500 text-center mt-8">
           Click on the Vite and React logos to learn more
         </p>
---
+++ src/tokens.ts
@@ -0,0 +1,8 @@
+export const tokens = {
+  container: "container mx-auto px-4 py-8",
+  card: "max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden p-6",
+  h1: "text-4xl font-bold text-gray-900 mb-8 text-center",
+  h2: "text-xl font-bold text-gray-900 my-4",
+  buttonPrimary:
+    "w-full bg-primary-500 hover:bg-primary-600 text-white font-semibold py-2 px-4 rounded-lg transition-colors duration-200",
+};
